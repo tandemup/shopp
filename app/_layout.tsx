1d2c68b65aebc892e21ca34b30ee167a166a544b
tandemup <ustaticdb@gmail.com>
2026-03-07 22:12:07 +0100

    Splash Screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,19 +2,36 @@ import DialogProvider from "@/components/ui/DialogProvider";
 import { ThemeProvider } from "@/hooks/useTheme";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { loadApp } from "../src/lib/loadApp";
+
+SplashScreen.preventAutoHideAsync().catch(() => {});
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
   unsavedChangesWarning: false,
 });
 
 export default function RootLayout() {
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await loadApp();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
+
   return (
     <ConvexProvider client={convex}>
       <ThemeProvider>
         <DialogProvider>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(tabs)" />
-          </Stack>
+          <Stack screenOptions={{ headerShown: false }} />
         </DialogProvider>
       </ThemeProvider>
     </ConvexProvider>
