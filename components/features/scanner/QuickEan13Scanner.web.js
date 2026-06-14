ca7747dcc90bc41406c57c246470df425f7a8538
tandemup <ustaticdb@gmail.com>
2026-06-14 19:44:17 +0200

    netlify15

--- components/features/scanner/QuickEan13Scanner.web.js
+++ components/features/scanner/QuickEan13Scanner.web.js
@@ -566,12 +566,8 @@ export default function QuickEan13ScannerWeb({
           facingMode: "environment",
         },
         {
-          fps: 18,
+          fps: 12,
           disableFlip: true,
-          qrbox: {
-            width: 360,
-            height: 180,
-          },
           aspectRatio: 1.7777778,
           experimentalFeatures: {
             useBarCodeDetectorIfSupported: true,
